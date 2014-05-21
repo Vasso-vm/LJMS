391e4c26af55b87e35d4002aa035be4c45ee1caa
Vasso-vm <vm.vasso@gmail.com>
2014-05-21 14:33:01 +0400

    add fixtures

---
+++ src/Ljms/CoreBundle/DataFixtures/ORM/LoadTypeUniformGroupData.php
@@ -0,0 +1,37 @@
+<?php
+namespace Ljms\CoreBundle\DataFixtures\ORM;
+
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Ljms\CoreBundle\Entity\TypeUniformGroup;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+class LoadTypeUniformGroupData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $types=array(
+            'Youth',
+            'Adult',
+        );
+        foreach ($types as $type){
+            $uniform= new TypeUniformGroup();
+            $uniform->setName($type);
+            $uniform->setIsActive(true);
+            $manager->persist($uniform);
+        }
+        $manager->flush();
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 1; // the order in which fixtures will be loaded
+    }
+}
+?>
---
+++ src/Ljms/CoreBundle/DataFixtures/ORM/LoadTypeUniformSizeData.php
@@ -0,0 +1,42 @@
+<?php
+namespace Ljms\CoreBundle\DataFixtures\ORM;
+
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Ljms\CoreBundle\Entity\TypeUniformSize;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+class LoadTypeUniformSizeData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $sizes=array(
+            'X-small',
+            'Small',
+            'Medium',
+            'Large',
+            'X-Large',
+            'XX-Large',
+            'XXX-Large',
+        );
+        foreach ($sizes as $size){
+            $uniform= new TypeUniformSize();
+            $uniform->setName($size);
+            $uniform->setIsActive(true);
+            $manager->persist($uniform);
+        }
+        $manager->flush();
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 1; // the order in which fixtures will be loaded
+    }
+}
+?>
